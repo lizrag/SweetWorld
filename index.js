d0d5f8a554def0850c1f6d7e4dc196f5df05e53d
Lizeth <perlizy.1996@gmail.com>
2022-06-02 22:28:49 -0500

    Add productroutes to index

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from "express";
-import productRoutes from "./routers/productRouter.js";
+import productRoutes from "./routers/product.routes.js";
 
 const app = express();
 const PORT = 8080;
 
-app.use('/', productRoutes);
+app.use('/api/', productRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
