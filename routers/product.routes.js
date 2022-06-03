ab018f76552fc7ae87d77ca0ce0ba3c1eac0f69a
Lizeth <perlizy.1996@gmail.com>
2022-06-02 21:13:30 -0500

    Add methods to the productroutes

--- routers/product.routes.js
+++ routers/product.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import ProductController from "../controllers/product.controller.js";
 
 
 const productRoutes = Router();
 
-productRoutes.get('/products',);
-productRoutes.get('/products/:product_id',);
-productRoutes.get('products?keyword=something',);
+productRoutes.get('/products', getProducts);
+productRoutes.get('/products/:product_id', getProductsId);
+productRoutes.get('products?keyword=something', getProductKeyword);
 
 
 
