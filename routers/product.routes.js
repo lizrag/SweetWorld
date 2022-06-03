b5b1ad4c1cc513f039919b8094311e498f90f0fa
Lizeth <perlizy.1996@gmail.com>
2022-06-02 20:52:28 -0500

    Add productroutes

---
+++ routers/product.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+
+const productRoutes = Router();
+
+productRoutes.get('/products',);
+productRoutes.get('/products/:product_id',);
+productRoutes.get('products?keyword=something',);
+
+
+
+export default productRoutes;
+
