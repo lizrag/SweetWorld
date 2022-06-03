43b1a6e195d0aff3f83d486cc28c34f287573163
Lizeth <perlizy.1996@gmail.com>
2022-06-02 21:10:58 -0500

    Add methods

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,6 +1,23 @@
 
 class ProductController{
-    
+
+    constructor(){
+        this.products = [];
+    }
+
+    getProduct= () => {
+
+    };
+
+    getProductId= ()=>{
+
+    }
+
+    getProductKeyword= ()=>{
+        
+    }
 };
+
+
 
 export default ProductController;
