8fa2ea95c411e373621cfe6c6a4e529496cb6bfa
Lizeth <perlizy.1996@gmail.com>
2022-06-07 19:53:04 -0500

    Add routes,controller and index of carts

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import productRoutes from "./routers/productRouter.js";
+import cartRoutes from "./routers/cartRouter.js";
 
 const app = express();
 const PORT = 8080;
 
-app.use('/', productRoutes);
+app.use('api/', productRoutes);
+app.use('api/', cartRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
