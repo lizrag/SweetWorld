8209dbb99a834afb9ade6933ae3ff8617cb1a815
Lizeth <perlizy.1996@gmail.com>
2022-06-07 21:48:33 -0500

    Add user controller, routes & index

---
+++ controllers/user.controller.js
@@ -0,0 +1,17 @@
+class UserController {
+    constructor() {
+      this.users = [];
+    }
+  
+    getUsers = (req, res) => {};
+  
+    getCartsByUserId = (req, res) => {};
+  
+    getCartsByUserIdAndStatus = (req, res) => {};
+  
+    createUser = (req, res) => {};
+  
+    updateUser = (req, res) => {};
+  }
+  
+  export default new UserController();
---
+++ routes/user.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import userController from "../controllers/user.controller.js";
+
+const userRoutes = Router();
+
+userRoutes.get("/users", userController.getUsers);
+userRoutes.get("/users/:user_id/carts", userController.getCartsByUserId);
+userRoutes.get("/users/:user_id/:status", userController.getCartsByUserIdAndStatus);
+userRoutes.post("/users/create", userController.createUser);
+
+userRoutes.put("/users/:user_id", userController.updateUser);
+
+export default userRoutes;
