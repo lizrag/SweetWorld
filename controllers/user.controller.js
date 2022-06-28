9c22dde59521f0416bbb3e4a77931bbd6a367ac3
Lizeth <perlizy.1996@gmail.com>
2022-06-28 16:18:54 -0700

    Add user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,4 @@
-import Users from "../models/user.schema.js"; 
+import Users from "../models/user.schema.js";
 import Carts from "../models/cart.schema.js";
 
 class UserController {
@@ -7,43 +7,48 @@ class UserController {
   }
 
   getUsers = async (req, res) => {
-    try{
+    try {
       const arrayUsers = await Users.find();
       return res.json(arrayUsers);
-    }catch(error){
+    } catch (error) {
       return res.json({ success: false, message: error.message });
     }
   };
 
   getCartsByUserId = async (req, res) => {
-    const userId = req.locals.user._id;
-    try{
-      const userCarts = await Carts.find({user_id: req.params.user_id}.populate({_id:cart_id}));
+    try {
+      const userCarts = await Carts.find({ user_id: req.params.user_id });
       return res.json(userCarts);
-    }catch(error){
+    } catch (error) {
       return res.json({ success: false, message: error.message });
     }
   };
 
   createUser = async (req, res) => {
-    let {name, last_name, email, password, address} = req.body;
+    let { name, last_name, email, password, address } = req.body;
 
-    try{
-      const newUser = await Users.create({name,last_name,email,password,address})
+    try {
+      const newUser = await Users.create({
+        name,
+        last_name,
+        email,
+        password,
+        address,
+      });
       return res.json(newUser);
-    }catch(error){
+    } catch (error) {
       return res.json({ success: false, message: error.message });
     }
   };
 
   updateUser = async (req, res) => {
     const user_id = req.locals.user._id;
-    try{
+    try {
       const user = req.params.user_id;
       const update = req.body;
-      const userUpdated = await Users.findByIdAndUpdate(user,user_id, update);
-      return res.send(userUpdated)
-    }catch(error){
+      const userUpdated = await Users.findByIdAndUpdate(user, user_id, update);
+      return res.send(userUpdated);
+    } catch (error) {
       return res.json({ success: false, message: error.message });
     }
   };
