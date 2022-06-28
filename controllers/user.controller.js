c3acdf03c648d759563b759992772f021d40fb7c
Lizeth <perlizy.1996@gmail.com>
2022-06-27 21:50:22 -0700

    Add methods user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,15 +1,52 @@
+import Users from "../models/user.schema.js"; 
+import Carts from "../models/cart.schema.js";
+
 class UserController {
   constructor() {
     this.users = [];
   }
 
-  getUsers = (req, res) => {};
+  getUsers = async (req, res) => {
+    try{
+      const arrayUsers = await Users.find();
+      return res.json(arrayUsers);
+    }catch(error){
+      return res.json({ success: false, message: error.message });
+    }
+  };
 
-  getCartsByUserId = (req, res) => {};
+  getCartsByUserId = async (req, res) => {
+    const userId = req.locals.user._id;
+    try{
+      const userCarts = await Carts.find({user_id: req.params.user_id}.populate({_id:cart_id}));
+      return res.json(userCarts);
+    }catch(error){
+      return res.json({ success: false, message: error.message });
+    }
+  };
 
-  createUser = (req, res) => {};
+  createUser = async (req, res) => {
+    let {name, last_name, email, password, address} = req.body;
 
-  updateUser = (req, res) => {};
+    try{
+      const newUser = await Users.create({name,last_name,email,password,address})
+      return res.json(newUser);
+    }catch(error){
+      return res.json({ success: false, message: error.message });
+    }
+  };
+
+  updateUser = async (req, res) => {
+    const user_id = req.locals.user._id;
+    try{
+      const user = req.params.user_id;
+      const update = req.body;
+      const userUpdated = await Users.findByIdAndUpdate(user,user_id, update);
+      return res.send(userUpdated)
+    }catch(error){
+      return res.json({ success: false, message: error.message });
+    }
+  };
 }
 
 export default new UserController();
